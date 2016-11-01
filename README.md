36b4715f4eaea8a8faea71b6fc745b43b6cdc33c
Daniel Dihardja <daniel@dihardja.de>
2016-11-01 14:07:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+##CouchDB Cors Settings
+    credentials: true
+    headers: accept, authorization, content-type, origin, referer, x-csrf-token
+    methods: GET, POST, PUT, DELETE, OPTIONS, HEAD
+    origins: *
+    
+    
+##Dependencies
+PouchDB
+PouchDB Authentication
---
+++ pouch-demo/flip-pouch.js
@@ -0,0 +1,94 @@
+/**
+ * Created by danieldihardja on 01/11/16.
+ */
+(function() {
+
+	angular.module('flip-pouch', [])
+		.provider('$flipPouch', flipPouch);
+
+	flipPouch.$inject = [];
+
+	function flipPouch() {
+
+		var _this = this;
+
+		var _dbRemote;
+		var _dbLocal;
+
+		var _remoteCouchDBUrl;
+		var _username;
+		var _password;
+		var _dbName;
+
+		this.setRemoteCouchDB = function(remoteUrl) {
+			_remoteCouchDBUrl = remoteUrl;
+		};
+
+		this.setUsername = function(username) {
+			_username = username;
+		};
+
+		this.setPassword = function(password) {
+			_password = password;
+		};
+
+		this.setDBName = function(name) {
+			_dbName = name;
+		};
+
+		this.$get = ['$q', function($q) {
+			_this.$q = $q;
+			return {
+				sync: sync,
+				wissensspeicher: {
+					themeByDotId: wsThemeByDotId
+				}
+			}
+		}];
+
+
+		//--------------------------//
+		// Helper functions
+		//--------------------------//
+
+		function sync() {
+			var defer = _this.$q.defer();
+			var ajaxOptions = {
+				ajax: {
+					headers: {
+						Authorization: 'Basic ' + window.btoa(_username+ ':' + _password)
+					}
+				}
+			};
+
+			_dbRemote = new PouchDB(_remoteCouchDBUrl, {skipSetup: true});
+			_dbRemote.login(_username, _password, ajaxOptions, function(err, res) {
+
+				if(err) {
+					defer.reject(err);
+					return;
+				}
+
+				_dbLocal = new PouchDB(_dbName);
+
+				PouchDB.replicate(_remoteCouchDBUrl, _dbName)
+					.on('complete', function(info) {
+						defer.resolve(info);
+					})
+					.on('error', function(err) {
+
+						// an error could be no internet connection.
+						// therefore just go on
+
+						defer.resolve();
+					})
+			});
+
+			return defer.promise;
+		}
+
+		function wsThemeByDotId(dotId) {
+			return _dbLocal.query('WSThema/byDotId', {key: dotId})
+		}
+	}
+})();
